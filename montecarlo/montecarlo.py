564f4657d131cdf7afb81a798271babc7372c53e
Sergio Abreu García <sagdev0@gmail.com>
2019-09-26 17:59:13 +0200

    el montecarlo

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,10 +1,13 @@
+# Diego Martínez Simarro y Sergio Abreu García
+
 import math
 import random
 import scipy.integrate as integrate
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
-def integra_mc_iterativa(fun, a, b, num_puntos=10000):
+def integra_mc_iterativa(fun, a, b, num_puntos=1000):
 
     #VARIABLE PARA CONTROLAR EL TIEMPO
     tic = time.time()
@@ -39,26 +42,27 @@ def integra_mc_iterativa(fun, a, b, num_puntos=10000):
     toc = time.time()
     print("Tiempo: ", 1000*(toc - tic))
 
-def integra_mc_vectores(fun, a, b, num_puntos=10000):
+    return 1000*(toc - tic)
+
+def integra_mc_vectores(fun, a, b, num_puntos=1000):
     
     #VARIABLE PARA CONTROLAR EL TIEMPO
     tic = time.time()
 
-   #HALLAMOS M
-    vector = np.arange(a, b, (b-a)/num_puntos)
+    #CREAMOS VECTORES CON PUNTOS
+    vectorX = np.random.uniform(low = a, high = b, size =(num_puntos))
 
+    #HALLAMOS M
     vectorizedFun = np.vectorize(fun)
-    M = vectorizedFun(vector).max()
+    funResults = vectorizedFun(vectorX)
+    M = funResults.max()
+    vectorY = np.random.uniform(low = 0, high = M, size =(num_puntos))
 
     print("\n__Integral vectorial__")
     print("Máximo: ", M)
-    
-    #CREAMOS VECTORES CON PUNTOS
-    vectorX = np.random.uniform(low = a, high = b, size =(num_puntos))
-    vectorY = np.random.uniform(low = 0, high = M, size =(num_puntos))
 
     #CONTAMOS LOS PUNTOS DEBAJO
-    nDebajo = np.greater(vectorizedFun(vectorX), vectorY).sum()
+    nDebajo = np.greater(funResults, vectorY).sum()
 
     print("N Debajo: ", nDebajo)
     print("N Totales: ", num_puntos)
@@ -70,7 +74,24 @@ def integra_mc_vectores(fun, a, b, num_puntos=10000):
     toc = time.time()
     print("Tiempo: ", 1000*(toc - tic))
 
-integra_mc_iterativa(math.sin, 0, math.pi)
-integra_mc_vectores(math.sin, 0, math.pi)
+    return 1000*(toc - tic)
+
+#LLAMAMOS VARIAS VECES A AMBAS FUNCIONES Y RELLENAMOS UNA GRAFICA CON SUS TIEMPOS
+iterativeArray = []
+vectorialArray = []
+iValues = []
+
+for i in range(1, 1000000, 100000):
+    iterativeArray.append(integra_mc_iterativa(math.sin, 0, math.pi, i))
+    vectorialArray.append(integra_mc_vectores(math.sin, 0, math.pi, i))
+    iValues.append(i)
+
+plt.figure()
+plt.scatter(iValues, iterativeArray, color='red', label="Iterative")
+plt.scatter(iValues, vectorialArray, color='blue', label="Recursive")
+plt.ylabel('Time comparison')
+plt.legend()
+plt.savefig("Grafica.png")
+plt.show()
 
 print("\nI real: ", integrate.quad(math.sin, 0, math.pi)[0], "\n")
