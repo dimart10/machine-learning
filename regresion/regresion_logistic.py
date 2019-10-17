a63e01df6a7f540a5f3e50a7d0c3734a7088677f
Sergio Abreu García <sagdev0@gmail.com>
2019-10-17 18:02:22 +0200

    Logistic regression

--- regresion/regresion_logistic.py
+++ regresion/regresion_logistic.py
@@ -1,55 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas.io.parsers import read_csv
+import scipy.optimize as opt
 
 def load_csv(file_name):
     values = read_csv(file_name, header=None).values
 
     return values.astype(float)
 
-def gradient(XX, Y, thetas):
-    H = h(XX, thetas)
+def gradient(thetas, XX, Y):
+    H = h(thetas, XX)
     grad = (1/len(Y)) * np.dot(XX.T, H-Y)
 
     return grad
 
-def cost(X, Y, thetas):
+def cost(thetas, X, Y):
     m = np.shape(X)[0]
-    H = h(X, thetas)
+    H = h(thetas, X)
 
-    c *= (- 1 / m) * np.dot(Y, np.log(H)) +  np.dot((1 - Y), np.log(1 - H))
+    c = (-1/(len(X))) * (np.dot(Y.T, np.log(H)) + np.dot((1-Y).T, np.log(1-H)))
 
     return c
 
 def sigmoid(Z):
-    return 1/(1 + np.exp(-Z))
+    return 1/(1 + np.e**(-Z))
 
-def h(X, thetas):
-    return sigmoid(np.dot(X, thetas))
+def h(thetas, X):
+    return np.c_[sigmoid(np.dot(X, thetas))]
 
-def show_border(X, Y, theta):
-    plt.figure()
-
-    pos = np.where(Y == 1)
-    plt.scatter(X[pos, 0], X[pos, 1], marker='+', c='k')
-
-    x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
-    x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
+def show_border(thetas, X, Y):
+    x1_min, x1_max = X[:, 1].min(), X[:, 1].max()
+    x2_min, x2_max = X[:, 2].min(), X[:, 2].max()
 
     xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
     np.linspace(x2_min, x2_max))
-
-    h = sigmoid(np.c_[np.ones((xx1.ravel().shape[0], 1)),
+    
+    H = sigmoid(np.c_[np.ones((xx1.ravel().shape[0], 1)),
     xx1.ravel(),
-    xx2.ravel()].dot(theta))
-    h = h.reshape(xx1.shape)
+    xx2.ravel()].dot(thetas))
 
-    # el cuarto parametro es el valor de z cuya frontera se
-    # quiere pintar
-    plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='b')
+    H = H.reshape(xx1.shape)
+
+    plt.figure()
+
+    positives = np.where(Y == 1)
+    negatives = np.where(Y == 0)
+    plt.scatter(X[positives, 1], X[positives, 2], marker='+', color='blue')
+    plt.scatter(X[negatives, 1], X[negatives, 2], color='red')
+
+    plt.contour(xx1, xx2, H, [0.5], linewidths=1, colors='b')
     plt.savefig("images/regresion_logistic_border.png")
     plt.show()
     plt.close()
+
+def evaluate(thetas, X, Y):
+    result = h(thetas, X)
+    passed_missed = np.logical_and((result >= 0.5), (Y == 0)).sum()
+    failed_missed = np.logical_and((result < 0.5), (Y == 1)).sum()
+
+    errors = (passed_missed + failed_missed)
+
+    return errors/(result.shape[0])
 
 def main():
     # DATA PREPROCESSING
@@ -58,12 +69,16 @@ def main():
     Y = datos[:, -1][np.newaxis].T
 
     m = np.shape(X)[0]
-    #X = np.hstack([np.ones([m, 1]), X])
+    X = np.hstack([np.ones([m, 1]), X])
     n = np.shape(X)[1]
 
     thetas = np.zeros((n, 1), dtype=float)
 
-    show_border(X, Y, thetas)
+    result = opt.fmin_tnc(func=cost, x0=thetas, fprime=gradient, args=(X, Y))
+    thetas = result[0]
+
+    print("Error percentage: ", evaluate(thetas, X, Y)*100, "%")
+    show_border(thetas, X, Y)
 
 
 if __name__ == "__main__":
