b13ab5736813b2ac815c666de026b4d3f999b48c
Sergio Abreu García <saggamedeveloper@gmail.com>
2019-11-18 21:22:15 +0100

    back prop created, not working

--- neural_networks/neural_network.py
+++ neural_networks/neural_network.py
@@ -9,33 +9,41 @@ def forward_propagation(X, thetas):
         # Input layer
         a = np.hstack([np.ones([m, 1]), X])
 
+        # Neural net parameters storage
+        A = [a]
+        Z = []
+
         # Hidden layers
         for i in range(0, len(thetas) - 1):
                 z = np.dot(a, thetas[i].T)
                 a = np.hstack([np.ones([m, 1]), sigmoid(z)])
+                A.append(a)
+                Z.append(z)
 
         # Output layer
         z = np.dot(a, thetas[-1].T)
+        Z.append(z)
         h = sigmoid(z)
 
         prediction = np.argmax(h, 1)
 
-        return np.reshape(prediction, (5000, 1))
+        return (A, Z, h, np.reshape(prediction, (5000, 1)))
 
-def backwards_propagation():
+def backwards_propagation(thetas, X, Y, reg):
     m = X.shape[0]
-    ...
-    a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
-    ...
-    for t in range(m):
-     a1t = a1[t, :] # (1, 401)
-     a2t = a2[t, :] # (1, 26)
-     ht = h[t, :] # (1, 10)
-     yt = y[t] # (1, 10)
-     d3t = ht - yt # (1, 10)
-     d2t = np.dot(theta2.T, d3t) * (a2t * (1 - a2t)) # (1, 26)
-     delta1 = delta1 + np.dot(d2t[1:, np.newaxis], a1t[np.newaxis, :])
-     delta2 = delta2 + np.dot(d3t[:, np.newaxis], a2t[np.newaxis, :])
+    A, Z, h = forward_propagation(X, thetas)[:-1]
+
+    for example in range(m):
+        last_d = h[example, :] - Y[example]
+        for layer in range(len(A[:-1]), -1, -1):
+            theta = thetas[layer]
+            a = A[layer][example, :]
+            d = np.dot(theta.T, last_d) * (a * (1 - a)) # (1, 26)
+            print(theta.shape)
+            theta += np.dot(last_d[1:, np.newaxis], a[np.newaxis, :])
+            last_d = d
+
+    return thetas
 
 def sigmoid(Z):
     return 1/(1 + np.e**(-Z))
@@ -51,8 +59,12 @@ def main():
 
         X, Y = data['X'], data['y']
         theta1, theta2 = weights['Theta1'], weights['Theta2']
-        prediction = forward_propagation(X, np.array([theta1, theta2]))
-        evaluate(prediction, Y)
+        thetas = np.array([theta1, theta2])
+
+        backwards_propagation(thetas, X, Y, 0)
+
+        #prediction = forward_propagation(X, thetas)[-1]
+        #evaluate(prediction, Y)
 
 if __name__ == "__main__":
         main()
